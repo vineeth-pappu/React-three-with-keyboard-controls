5a3082f3e57d617a87841298686366628b1a79f5
Vineeth Viswanathan <vineeth0394@gmail.com>
2020-10-26 14:37:29 +0530

    Use rotateAroundWorldAxis instead of direct object rotation

--- src/App.js
+++ src/App.js
@@ -51,18 +51,47 @@ const Core = () => {
   useFrame(() => {
     // group.current.rotation.y += 0.005;
     if (keyLeftPressed) {
-      group.current.rotation.y += 0.01;
+      // group.current.rotation.y += 0.01;
+      var yAxis = new THREE.Vector3(0, 1, 0);
+      rotateAroundWorldAxis(group.current, yAxis, 1 * (Math.PI / 180));
     }
     if (keyRightPressed) {
-      group.current.rotation.y -= 0.01;
+      // group.current.rotation.y -= 0.01;
+      var yAxis = new THREE.Vector3(0, -1, 0);
+      rotateAroundWorldAxis(group.current, yAxis, 1 * (Math.PI / 180));
     }
     if (keyUpPressed) {
-      group.current.rotation.x -= 0.01;
+      // group.current.rotation.x -= 0.01;
+      var xAxis = new THREE.Vector3(1, 0, 0);
+      rotateAroundWorldAxis(group.current, xAxis, 1 * (Math.PI / 180));
     }
     if (keyDownPressed) {
-      group.current.rotation.x += 0.01;
+      // group.current.rotation.x += 0.01;
+      var xAxis = new THREE.Vector3(-1, 0, 0);
+      rotateAroundWorldAxis(group.current, xAxis, 1 * (Math.PI / 180));
     }
   });
+
+  var rotWorldMatrix;
+  // Rotate an object around an arbitrary axis in world space
+  const rotateAroundWorldAxis = (object, axis, radians) => {
+    rotWorldMatrix = new THREE.Matrix4();
+    rotWorldMatrix.makeRotationAxis(axis.normalize(), radians);
+
+    // old code for Three.JS pre r54:
+    //  rotWorldMatrix.multiply(object.matrix);
+    // new code for Three.JS r55+:
+    rotWorldMatrix.multiply(object.matrix); // pre-multiply
+
+    object.matrix = rotWorldMatrix;
+
+    // old code for Three.js pre r49:
+    // object.rotation.getRotationFromMatrix(object.matrix, object.scale);
+    // old code for Three.js pre r59:
+    // object.rotation.setEulerFromRotationMatrix(object.matrix);
+    // code for r59+:
+    object.rotation.setFromRotationMatrix(object.matrix);
+  };
 
   return (
     <mesh ref={group}>
@@ -81,7 +110,7 @@ const Core = () => {
 const App = () => {
   return (
     <Fragment>
-      <Canvas camera={{ position: [0, 0, 70] }}>
+      <Canvas camera={{ position: [0, 0, 1] }}>
         <ambientLight />
         <pointLight color="orange" intensity={10} />
         <Cubes />
